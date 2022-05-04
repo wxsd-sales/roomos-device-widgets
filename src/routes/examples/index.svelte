fee7a4e46c80071a9534ff8738eb65196f940266
Ashesh Singh <ask@mrsinh.com>
2022-05-04 13:43:28 -0500

    feat: add example layout and route

---
+++ src/routes/examples/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Example from '../../components/Example.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Examples</p>
+<Example />
