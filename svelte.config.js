66c42a3923de9c7566fb0892b29cca6ad1f6f066
Ashesh Singh <ask@mrsinh.com>
2022-05-06 21:19:37 -0500

    build: export project's package information

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { fileURLToPath } from 'url';
+import { readFileSync } from 'fs';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+export const pkg = JSON.parse(json);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
