91b6114b6432ffea0a5f7983fde836aa4fc523c0
Ashesh Singh <ask@mrsinh.com>
2022-05-06 21:14:05 -0500

    feat: update primary index route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container">
+  <div class="columns is-centered is-mobile my-1 py-6">
+    <div class="column is-four-fifths">
+      <div class="box mb-4 pb-4">
+        <h1 class="title">Welcome to SvelteKit</h1>
+        <div class="card-content">
+          <div class="content">
+            <p>
+              Visit <a href="https://kit.svelte.dev" target="_blank" rel="noreferrer">kit.svelte.dev</a> to read the documentation
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
