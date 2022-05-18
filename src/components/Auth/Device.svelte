fa1e33d8589a7e65f006c552a5e950bdbe1c6f33
Ashesh Singh <ask@mrsinh.com>
2022-05-18 08:31:32 -0500

    feat: add get new token button

--- src/components/Auth/Device.svelte
+++ src/components/Auth/Device.svelte
@@ -10,6 +10,7 @@
   let webexDeviceValidateUrl: string;
   let qrImage: HTMLImageElement;
   let tries = 0;
+  let expired = true;
 
   const jsonRequest = new JsonRequest('/actions', 'device');
   const sha = new jsSHA('SHA3-256', 'TEXT', { encoding: 'UTF8' });
@@ -40,6 +41,7 @@
       if (tries >= expiresIn / interval || $webexOauthSessionWritable) {
         console.info('Clearing setInterval id:', id);
         clearInterval(id);
+        expired = true;
       }
 
       token(deviceCode)
@@ -58,18 +60,19 @@
       .then(() => updateQrCode(webexDeviceValidateUrl, qrImage))
       .then(() =>
         pollTokenEndpoint(authorizeResponse.device_code, authorizeResponse.interval, authorizeResponse.expires_in)
-      );
+      )
+      .finally(() => (expired = false));
   }
   onMount(() => init());
 </script>
 
-<div class="columns">
-  <div class="column is-two-fifths">
+<div class="columns is-multiline v-ce">
+  <div class="column is-two-fifths" class:is-invisible={expired}>
     <figure class="has-text-centered">
       <img bind:this={qrImage} id="qr-code" alt="Webex QR Code" src="" />
     </figure>
   </div>
-  <div class="column has-text-centered-mobile is-align-items-center is-flex-tablet">
+  <div class="column has-text-centered-mobile is-align-items-center is-flex-tablet" class:is-invisible={expired}>
     <div>
       <p class="is-size-4">Can't scan the QR code?</p>
       <br />
@@ -82,10 +85,18 @@
       <code id="user-code" class="subtitle has-text-weight-semibold">{authorizeResponse?.user_code}</code>
     </div>
   </div>
+  {#if authorizeResponse && !$webexOauthSessionWritable?.access_token && expired === false}
+    <div class="column is-12 has-text-centered" class:is-hidden={expired}>
+      Expires in {(authorizeResponse.expires_in / authorizeResponse.interval).toFixed(0) - tries} seconds
+    </div>
+  {:else}
+    <div class="column is-12 has-text-centered">
+      <button class="button is-active is-info is-rounded is-large" on:click={init}>
+        <span class="icon">
+          <i class="mdi mdi-qrcode-plus" />
+        </span>
+        <span>Get New Code</span>
+      </button>
+    </div>
+  {/if}
 </div>
-
-{#if authorizeResponse && !$webexOauthSessionWritable?.access_token}
-  <div class="column is-12 has-text-centered">
-    Expires in {(authorizeResponse.expires_in / authorizeResponse.interval).toFixed(0) - tries} seconds
-  </div>
-{/if}
