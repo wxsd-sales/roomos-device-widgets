f8aa5f102a394af18afda6908d12a0207a416053
Arash Koushkebaghi <arash.koush@gmail.com>
2022-05-27 10:31:14 -0700

    feat: add page

--- src/routes/weather/index.ts
+++ src/routes/weather/index.ts
@@ -7,12 +7,19 @@ const owmWeatherApiKey: string = process.env.OPENWEATHERMAP_API_KEY || '';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 export async function get(request) {
+  const query = request.url.searchParams.get('id')
+    ? {
+        appid: owmWeatherApiKey,
+        id: request.url.searchParams.get('id'),
+        units: request.url.searchParams.get('units') || 'imperial'
+      }
+    : {
+        appid: owmWeatherApiKey,
+        q: request.url.searchParams.get('city')
+      };
+
   return jsonRequest(owmWeatherApiUrl, 'weather')
-    .get(undefined, {
-      appid: owmWeatherApiKey,
-      id: request.url.searchParams.get('id'),
-      units: request.url.searchParams.get('units') || 'imperial'
-    })
+    .get(undefined, query)
     .then((r) => ({ body: r }))
     .catch(() => ({ status: 400 }));
 }
