330bdfc1d423e58ef2091ca2f1ecac1e2f2e16a0
Ashesh Singh <ask@mrsinh.com>
2022-06-11 21:10:04 -0500

    feat: add error page

---
+++ src/components/Error/Error.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let status = 500;
+  export let error: Error | null = undefined;
+</script>
+
+<section class="container is-error-container">
+  <div class="columns is-multiline">
+    <div class="column is-12">
+      <pre class="title is-size-1 has-text-danger">Error: {status}</pre>
+    </div>
+    <div class="column is-12" class:is-hidden={error?.message == null}>
+      <pre class="subtitle">{error?.message}</pre>
+    </div>
+    <div class="column is-12" class:is-hidden={error?.stack == null}>
+      <pre>{error?.stack}</pre>
+    </div>
+    <slot />
+    <div class="column is-12">
+      <a class="button is-link is-large is-fullwidth is-rounded" href="/">
+        <span class="icon is-large">
+          <i class="mdi mdi-arrow-left" />
+        </span>
+        <span>Home</span>
+      </a>
+    </div>
+  </div>
+</section>
+
+<style>
+  pre {
+    max-width: 90vw;
+    border-radius: 0.5rem;
+    white-space: pre-wrap;
+  }
+</style>
---
+++ src/routes/__error.svelte
@@ -0,0 +1,89 @@
+<script context="module" lang="ts">
+  import type { Load, LoadEvent } from '@sveltejs/kit';
+
+  export const load: Load = (loadEvent: LoadEvent) => {
+    const { params, props, routeId, session, stuff, url, status, error } = loadEvent;
+
+    return {
+      props: {
+        params: params,
+        props: import.meta.env.DEV ? props : null,
+        routeId: import.meta.env.DEV ? routeId : null,
+        session: session,
+        stuff: import.meta.env.DEV ? stuff : null,
+        url: url,
+        status: status,
+        error: import.meta.env.DEV ? error : null
+      }
+    };
+  };
+</script>
+
+<script lang="ts">
+  import ErrorComponent from '../components/Error/Error.svelte';
+
+  export let params = undefined;
+  export let props = undefined;
+  export let routeId = undefined;
+  export let session = undefined;
+  export let stuff = undefined;
+  export let url = undefined;
+  export let status = undefined;
+  export let error = undefined;
+</script>
+
+<ErrorComponent {status} {error}>
+  {#if import.meta.env.DEV}
+    <div class="column is-12">
+      <details open>
+        <summary>params</summary>
+        <pre>{JSON.stringify(params, null, 2)}</pre>
+      </details>
+    </div>
+    <div class="column is-12">
+      <details open>
+        <summary>props</summary>
+        <pre>{JSON.stringify(props, null, 2)}</pre>
+      </details>
+    </div>
+    <div class="column is-12">
+      <details open>
+        <summary>routeId</summary>
+        <pre>{JSON.stringify(routeId, null, 2)}</pre>
+      </details>
+    </div>
+    <div class="column is-12">
+      <details open>
+        <summary>session</summary>
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+      </details>
+    </div>
+    <div class="column is-12">
+      <details open>
+        <summary>stuff</summary>
+        <pre>{JSON.stringify(stuff, null, 2)}</pre>
+      </details>
+    </div>
+    <div class="column is-12">
+      <details open>
+        <summary>url</summary>
+        <pre>{JSON.stringify(url, null, 2)}</pre>
+      </details>
+    </div>
+  {/if}
+</ErrorComponent>
+
+<style>
+  pre {
+    max-height: 400px;
+    border-radius: var(--border-radius);
+  }
+  details > summary {
+    cursor: pointer;
+  }
+  details {
+    border: 1px solid hsl(0deg, 0%, 86%);
+    border-radius: var(--border-radius);
+    padding: 0.75rem;
+  }
+</style>
