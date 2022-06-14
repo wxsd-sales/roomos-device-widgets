9f6e208e83242778d7159fd123a4697e78beec8d
Ashesh Singh <ask@mrsinh.com>
2022-06-13 21:10:04 -0500

    feat: add background component

---
+++ src/components/Background/Background.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let filter = 'brightness(55%)';
+  export let imageLink: string | URL = undefined;
+  export let videoLink: string | URL = undefined;
+</script>
+
+{#if imageLink && videoLink}
+  <video class="is-brand-background" autoplay loop muted playsinline poster={imageLink} style:filter>
+    <source src={videoLink} />
+  </video>
+{:else}
+  <img class="is-brand-background" alt="background" src={imageLink} style:filter on:load />
+{/if}
+
+<style>
+  .is-brand-background {
+    object-fit: cover;
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top: 0;
+    left: 0;
+  }
+</style>
