1d1a2087fdb17787282d95717c5e7a1309e79c56
Ashesh Singh <ask@mrsinh.com>
2022-06-16 21:10:04 -0500

    feat: add news api

---
+++ src/routes/api/news/index.ts
@@ -0,0 +1,31 @@
+import type { RequestEvent, RequestHandler } from '@sveltejs/kit';
+import { urlEncodedRequest } from '$lib/shared/urlencoded-request';
+import env from '$lib/environment';
+
+/**
+ * @param requestEvent
+ *
+ * @returns {Promise<
+ *   | {
+ *       headers: { 'Cache-Control': 'public, max-age=1800'; 'Expires': string; 'Content-Type': 'application/xml' };
+ *       body: string;
+ *       status: 200;
+ *     }
+ *   | { status: 500 }
+ * >}
+ */
+export const GET: RequestHandler = async (requestEvent: RequestEvent) => {
+  return urlEncodedRequest(env.GOOGLE_NEWS_RSS_URL)
+    .get(undefined, { q: requestEvent.url.searchParams.get('q') })
+    .then((r) => r.text())
+    .then((r) => ({
+      status: 200,
+      body: r,
+      headers: {
+        'Cache-Control': 'public, max-age=1800',
+        'Content-Type': 'application/xml',
+        'Expires': new Date(Date.now() + 30 * 60 * 1000).toUTCString()
+      }
+    }))
+    .catch(() => ({ status: 500 }));
+};
