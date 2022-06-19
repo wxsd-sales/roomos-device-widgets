ebb3ffc095d8170f1981736f044e79fe4fc05a7e
Ashesh Singh <ask@mrsinh.com>
2022-06-18 21:10:04 -0500

    refactor: update webex oauth devcie-code api

---
+++ src/routes/api/auth-webex/device-code/authorize.ts
@@ -0,0 +1,96 @@
+import 'reflect-metadata';
+import type { RequestEvent, RequestHandler } from '@sveltejs/kit';
+import type { JSONObject, JSONValue, ToJSON } from '@sveltejs/kit/types/private';
+import type { QRCodeRenderersOptions } from 'qrcode';
+import { urlEncodedRequest } from '$lib/shared/urlencoded-request';
+import { Expose, instanceToPlain, plainToInstance, Transform } from 'class-transformer';
+import { sha3 } from 'hash-wasm';
+import { toDataURL as qrCodeToDataUrl } from 'qrcode';
+import { classTransformOptions, onFailure } from '../../../.utils';
+import humps from 'humps';
+import env from '$lib/environment';
+
+/** @typedef {import('class-validator').ValidationError} ValidationError */
+
+/**
+ * @param {RequestEvent} requestEvent
+ *
+ * @returns {Promise<{ body: ResponseDTO; status: 200 } | { body: any; status: number } | { status: 500 }>}
+ */
+export const POST: RequestHandler = async (requestEvent: RequestEvent) => {
+  class ResponseDTO implements ToJSON {
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.device_code, { toClassOnly: true })
+    public readonly deviceCode!: string;
+
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.expires_in, { toClassOnly: true })
+    public readonly expiresIn!: number;
+
+    @Expose()
+    @Transform(({ value }) => new Date(value).toUTCString(), { toPlainOnly: true })
+    public readonly expiresAt?: number;
+
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.user_code, { toClassOnly: true })
+    public readonly userCode!: string;
+
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.verification_uri, { toClassOnly: true })
+    public readonly verificationUri!: string;
+
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.verification_uri_complete, { toClassOnly: true })
+    public readonly verificationUriComplete?: string;
+
+    @Expose()
+    public readonly interval!: number;
+
+    @Expose()
+    public readonly qrImage!: string;
+
+    toJSON(): Exclude<JSONValue, ToJSON> {
+      return instanceToPlain(this);
+    }
+  }
+
+  const addVerificationUriComplete = (obj: JSONObject) =>
+    obj?.verificationUriComplete != null
+      ? Promise.resolve(obj)
+      : sha3(obj.userCode as string, 256).then((r) => {
+          const url = new URL(env.WEBEX_OAUTH_HELP_SERVICE_URL + '/' + env.WEBEX_DEVICE_CODE_VALIDATE_ENDPOINT);
+          url.searchParams.get('userCode') || url.searchParams.set('userCode', r);
+          return { ...obj, verificationUriComplete: url.toString() };
+        });
+
+  const addQrImage = (obj: JSONObject, options: QRCodeRenderersOptions = { width: 500 }) =>
+    qrCodeToDataUrl(obj.verificationUriComplete as string, options).then((r) => ({ ...obj, qrImage: r }));
+
+  const addExpiresAt = (obj: JSONObject, date: number = Date.now()) => ({
+    ...obj,
+    expiresAt: date + (obj.expiresIn as number) * 1000
+  });
+
+  return urlEncodedRequest(env.WEBEX_OAUTH_HELP_SERVICE_URL)
+    .post(
+      env.WEBEX_DEVICE_CODE_AUTHORIZE_ENDPOINT,
+      undefined,
+      humps.decamelizeKeys({
+        clientId: env.WEBEX_DEVICE_CODE_CLIENT_ID,
+        scope: env.WEBEX_DEVICE_CODE_CLIENT_SCOPE
+      }) as JSONObject
+    )
+    .then((r: Response) =>
+      r
+        .json()
+        .then((s: JSONObject) => humps.camelizeKeys(s) as JSONObject)
+        .then((s: JSONObject) => addVerificationUriComplete(s))
+        .then((s: JSONObject) => addQrImage(s, { width: 240 }))
+        .then((s: JSONObject) => addExpiresAt(s, Date.parse(r.headers.get('date') ?? new Date().toUTCString())))
+    )
+    .then((r: JSONObject) => ({
+      status: 200,
+      body: plainToInstance(ResponseDTO, r, classTransformOptions)
+    }))
+    .catch((e) => onFailure(e));
+};
---
+++ src/routes/api/auth-webex/device-code/token.ts
@@ -0,0 +1,117 @@
+import 'reflect-metadata';
+import type { RequestEvent, RequestHandler } from '@sveltejs/kit';
+import type { JSONObject, JSONValue, ToJSON } from '@sveltejs/kit/types/private';
+import { urlEncodedRequest } from '$lib/shared/urlencoded-request';
+import { Expose, instanceToPlain, plainToInstance, Transform } from 'class-transformer';
+import { IsNotEmpty, IsUUID, validateSync } from 'class-validator';
+import humps from 'humps';
+import env from '$lib/environment';
+import { classTransformOptions, classValidationOptions } from '../../../.utils';
+
+/** @typedef {import('class-validator').ValidationError} ValidationError */
+
+const onFailure = async (e: Response | Error) => {
+  if (e instanceof Response && e.status !== 401) {
+    return e.status === 428
+      ? { status: e.status, body: await e.json(), headers: { 'Skip-Reporting': true } }
+      : { status: e.status, body: await e.json() };
+  } else {
+    console.log(e);
+    return { status: 500 };
+  }
+};
+
+/**
+ * @param {RequestEvent} requestEvent
+ *
+ * @returns {Promise<
+ *   | { body: { query: ValidationError[] }; status: 400 }
+ *   | { body: ResponseDTO; status: 200 }
+ *   | { body: any; status: number }
+ *   | { status: 500 }
+ * >}
+ */
+export const POST = async (requestEvent: RequestEvent) => {
+  class RequestQueryDTO {
+    @Expose()
+    @IsNotEmpty()
+    @IsUUID(4)
+    public readonly deviceCode!: string;
+  }
+
+  class ResponseDTO implements ToJSON {
+    @Expose()
+    public readonly scope!: string;
+
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.expires_in, { toClassOnly: true })
+    public readonly expiresIn!: number;
+
+    @Expose()
+    @Transform(({ value }) => new Date(value).toUTCString(), { toPlainOnly: true })
+    public readonly expiresAt?: number;
+
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.token_type, { toClassOnly: true })
+    public readonly tokenType!: string;
+
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.access_token, { toClassOnly: true })
+    public readonly accessToken!: string;
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.refresh_token, { toClassOnly: true })
+    public readonly refreshToken!: string;
+
+    @Expose()
+    @Transform(({ obj, value }) => value ?? obj.refresh_token_expires_in, { toClassOnly: true })
+    public readonly refreshTokenExpiresIn!: number;
+
+    @Expose()
+    @Transform(({ value }) => new Date(value).toUTCString(), { toPlainOnly: true })
+    public readonly refreshTokenExpiresAt?: number;
+
+    toJSON(): Exclude<JSONValue, ToJSON> {
+      return instanceToPlain(this);
+    }
+  }
+
+  const data = env.WEBEX_DEVICE_CODE_CLIENT_ID + ':' + env.WEBEX_DEVICE_CODE_CLIENT_SECRET;
+  const credentials = Buffer.from(data).toString('base64');
+
+  // validate request query
+  const searchParams = Object.fromEntries(requestEvent.url.searchParams);
+  const query = plainToInstance(RequestQueryDTO, searchParams, classTransformOptions);
+  const queryValidationErrors = validateSync(query, classValidationOptions);
+  if (queryValidationErrors.length > 0) {
+    return { status: 400, body: { query: queryValidationErrors } };
+  }
+
+  const addExpiresAts = (obj: JSONObject, date: number = Date.now()) => ({
+    ...{
+      expiresAt: date + (obj.expiresIn as number) * 1000,
+      refreshTokenExpiresAt: date + (obj.refreshTokenExpiresIn as number) * 1000
+    },
+    ...obj
+  });
+
+  return urlEncodedRequest(env.WEBEX_OAUTH_HELP_SERVICE_URL, undefined, 'Basic', credentials)
+    .post(
+      env.WEBEX_DEVICE_CODE_TOKEN_ENDPOINT,
+      humps.decamelizeKeys({
+        grantType: env.WEBEX_DEVICE_CODE_TYPE,
+        clientId: env.WEBEX_DEVICE_CODE_CLIENT_ID,
+        deviceCode: query.deviceCode
+      }) as JSONObject
+    )
+    .then((r: Response) =>
+      r
+        .json()
+        .then((s: JSONObject) => humps.camelizeKeys(s) as JSONObject)
+        .then((s: JSONObject) => addExpiresAts(s, Date.parse(r.headers.get('date') ?? new Date().toUTCString())))
+    )
+    .then((r: JSONObject) => ({
+      status: 200,
+      body: plainToInstance(ResponseDTO, r, classTransformOptions)
+    }))
+    .catch((e) => onFailure(e));
+};
