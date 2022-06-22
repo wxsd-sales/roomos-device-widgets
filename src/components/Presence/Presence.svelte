6911ffb8ce20db387aced771dc219bbad6e209f6
Ashesh Singh <ask@mrsinh.com>
2022-06-21 21:10:04 -0500

    feat: add presence component

---
+++ src/components/Presence/Presence.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { STATUS } from './constants/status';
+  import { PRESENCE } from './constants/presence';
+
+  export let id = undefined;
+  export let accessToken = undefined;
+  export let status: STATUS;
+
+  $: icon = PRESENCE[status].icon;
+  $: color = PRESENCE[status].color;
+
+  let size;
+</script>
+
+<span class="icon has-background-dark presence" bind:clientWidth={size} title={status}>
+  <i class="mdi mdi-{icon} has-text-{color}" style="font-size: {(size || 8) - 4}px" />
+</span>
+
+<style>
+  .presence > i {
+    padding-top: 6%;
+  }
+</style>
---
+++ src/components/Presence/constants/status.ts
@@ -0,0 +1,11 @@
+export enum STATUS {
+  ACTIVE = 'active',
+  CALL = 'call',
+  DND = 'DoNotDisturb',
+  INACTIVE = 'inactive',
+  MEETING = 'meeting',
+  OOO = 'OutOfOffice',
+  PENDING = 'pending',
+  PRESENTING = 'presenting',
+  UNKNOWN = 'unknown'
+}
