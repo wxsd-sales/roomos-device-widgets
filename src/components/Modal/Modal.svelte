838ccd764ff789063322251adeb7a2847930541f
Ashesh Singh <ask@mrsinh.com>
2022-06-21 21:10:04 -0500

    feat: add modal component

---
+++ src/components/Modal/Modal.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let isActive = true;
+</script>
+
+<div class="modal" class:is-active={isActive}>
+  <div class="modal-background" on:click />
+  <section class="modal-content">
+    <slot />
+  </section>
+</div>
