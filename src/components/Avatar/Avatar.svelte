70667ae8ba080d433c5079f69cd542afe3dcba38
Ashesh Singh <ask@mrsinh.com>
2022-06-22 21:10:04 -0500

    feat: add avatar component

---
+++ src/components/Avatar/Avatar.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import type { Status } from './type/status';
+  import Presence from '../Presence/Presence.svelte';
+
+  export let initials = 'U';
+  export let image = undefined;
+  export let status: Status = undefined;
+  export let size = 64;
+</script>
+
+<figure class="image is-{size}x{size} avatar-container">
+  <img class="is-rounded avatar-image" src={image ?? '/api/avatar?initials=' + initials} alt="avatar" />
+  {#if status && status !== 'unknown'}
+    <Presence {status} />
+  {/if}
+</figure>
+
+<style>
+  figure.avatar-container {
+    position: relative !important;
+  }
+
+  figure.avatar-container > .avatar-image {
+    position: absolute !important;
+    width: 100%;
+    height: 100%;
+    bottom: 0;
+    right: 0;
+  }
+
+  figure.avatar-container > :global(.presence) {
+    position: absolute !important;
+    border-radius: 100%;
+    width: 35%;
+    height: 35%;
+    bottom: 0;
+    right: 0;
+  }
+</style>
