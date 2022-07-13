60a8f8e035647c12a795b11e58521ac67885976e
Ashesh Singh <ask@mrsinh.com>
2022-07-12 21:10:04 -0500

    feat: add index exports

---
+++ src/lib/webex/http-wrapper/index.ts
@@ -0,0 +1,3 @@
+export { webexHttp } from './webex-http';
+export { webexHttpMessagesResource } from './webex-http-messages-resource';
+export { webexHttpPeopleResource } from './webex-http-people-resource';
---
+++ src/lib/webex/sdk-wrapper/index.ts
@@ -0,0 +1,3 @@
+export { WebexSdk } from './webex-sdk.js';
+export { WebexSdkMeetingsPlugin } from './webex-sdk-meetings-plugin.js';
+export { WebexSdkMessagesPlugin } from './webex-sdk-messages-plugin.js';
