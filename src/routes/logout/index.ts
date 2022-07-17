58fc994091e6a5ca44a1c111b6a7985a36ec06eb
Ashesh Singh <ask@mrsinh.com>
2022-07-16 21:10:04 -0500

    feat: add logout route

---
+++ src/routes/logout/index.ts
@@ -0,0 +1,24 @@
+import type { RequestEvent } from '@sveltejs/kit';
+import { MikroORM, wrap } from '@mikro-orm/core';
+import { Session, User } from '../../database/entities';
+import config from '../../../mikro-orm.config';
+import * as cookie from 'cookie';
+
+export const POST = async (requestEvent: RequestEvent) => {
+  const orm = await MikroORM.init({ ...config, ...{ entities: [User, Session] } });
+  const em = orm.em.fork();
+
+  if (requestEvent.locals.session?.uuid && requestEvent.locals.session.user?.uuid) {
+    const session = cookie.serialize('sessionId', '', { path: '/', maxAge: -1, sameSite: 'strict' });
+
+    return await em
+      .persistAndFlush(wrap(requestEvent.locals.session).assign({ isExpired: true }))
+      .then(() => ({
+        status: 302,
+        headers: { 'Location': '/login', 'Set-Cookie': session }
+      }))
+      .catch(() => ({ status: 422 }));
+  }
+
+  return { status: 422 };
+};
