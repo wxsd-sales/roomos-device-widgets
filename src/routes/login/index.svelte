3d84db9c18020ddcc46b732e7c5dd4ff6c1bf487
Ashesh Singh <ask@mrsinh.com>
2022-08-17 19:08:54 -0500

    feat: update login page messaging

--- src/routes/login/index.svelte
+++ src/routes/login/index.svelte
@@ -14,23 +14,21 @@
 
 <div id="login" class="p-2 is-flex is-align-items-center">
   <Login {form} {email} {action}>
-    <article class="message is-danger is-large">
+    <article class="message is-danger is-medium">
       <div class="message-header">
         <p>Private Preview Notice</p>
       </div>
       <div class="message-body content">
         <p>
-          You are using a private preview of this application (intended for limited audience and testing), data
+          You are using a limited preview of this application (intended for select audience and testing). Data
           persistence is not guaranteed. We will do a public release very soon, meanwhile please consider starring the
           project on <a {href} target="_blank">GitHub: wxsd-sales/roomos-device-widgets</a>.
+        </p>
+        <p>
+          If you need access, please reach out to
+          <a href="mailto:wxsd@external.cisco.com">wxsd@external.cisco.com</a> and state your use.
         </p>
       </div>
     </article>
   </Login>
 </div>
-
-<style>
-  #login {
-    height: 70vh;
-  }
-</style>
