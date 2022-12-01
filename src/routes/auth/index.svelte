3b24bb4f47433504f37782c7448c32d0690ffb48
Ashesh Singh <ask@mrsinh.com>
2022-12-01 15:27:21 -0600

    feat: add note for users to use webex sandbox environment (#30)

--- src/routes/auth/index.svelte
+++ src/routes/auth/index.svelte
@@ -22,16 +22,24 @@
             controls. To learn more, watch this
             <a href="https://app.vidcast.io/share/bb910329-f398-4f04-baec-18ddaf46f493" target="_blank">Vidcast</a>.
           </p>
+        </div>
+      </article>
+      <article class="message is-danger is-medium">
+        <div class="message-header is-hidden" />
+        <div class="message-body content">
           <p>
             Before continuing, please note that you will need access to a <b>compatible RoomOS 11 device</b> registered
             in <b>shared mode</b> and <b>administrator access to the corresponding Webex Control Hub</b> organization.
           </p>
           <ul>
             <li>
-              If you don't have admin access to any sandboxed or demo Control Hub org, please follow the instructions on
-              the
+              Do not login using your Cisco account (*@cisco.com); as you would most likely not have admin access to the
+              corresponding Control Hub org.
+            </li>
+            <li>
+              If you don't have an existing sandboxed or demo Control Hub org, please follow the instructions on the
               <a href="https://collabtoolbox.cisco.com/tools/sandbox" target="_blank">Collab Toolbox Sandbox</a> page to
-              create one.
+              create one as an admin.
             </li>
             <li>
               If your device is in personal mode, you will have to perform a factory reset and then register it as a
